9a363481982e396bc89fd186b07b07302cb64245
malikadebiyi <malikbiyi@gmail.com>
2023-11-07 19:36:08 -0500

    week 10 lab

---
+++ week8/week10_events_forms/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Hello from './components/Hello';
+import UserFormFunction from './components/UserFormFunctions';
+import UserFormClass from './components/UserFormClass';
+function App() {
+  return (
+    <div>
+      <h1>Week 10 - Events and Forms</h1>
+      <Hello />
+      <UserFormClass />
+      <UserFormFunction />
+
+      </div>
+  );
+}
+
+export default App;
